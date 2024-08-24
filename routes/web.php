aef3593729e20a83eead427799be10e2897edc41
Hugo Pereira Oliveira <hugo.pe.po@hotmail.com>
2024-08-24 19:09:57 -0300

    Adicionado primeiras rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\{
+    PrincipalController,
+    SobreNosController,
+    ContatoController,
+    FornecedorController,
+    TesteController
+};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::match(['get', 'post'], '/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.teste');
+
+Route::fallback(function() {
+    return 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para a página inicial';
 });
